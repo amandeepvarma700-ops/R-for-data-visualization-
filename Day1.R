a9a386ee7fc4160d4ff26531b66c4f5489b70bd8
Student- Amandeep Varma <amandeepvarma700@gmail.com>
2026-07-29 16:17:12 +0530

    Day1

--- Day1.R
+++ Day1.R
@@ -4,9 +4,37 @@ sum2 = 78+33
 print(sum2)
 sum3<-44;sum4<-55
 
-# this is Rstdio 
+# basic plot
 x<-1:10
 plot(x)
 
+# R is case sensitive
+a<-10
+A<-11
 
+ls() #to check the the variables stroed
 
+rm(a) #we can remove an objedt usig this
+print(a) # will give error
+
+y.1<-14
+y.2<-22
+print(y.1)
+
+xx <- "marin"
+print(xx)
+print(x+y.1) #will add 14 to each element of array
+
+x<-9
+y<-4
+#for power use ^
+print(x^2 + y^2)
+#for sqrt use it as funciton
+print(sqrt(x)+sqrt(y))
+
+#also has log and exp
+y = log(y) #taking log of y
+print(y)
+y = exp(y) #taking anti log of y
+
+print(abs(-14))
